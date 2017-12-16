9914ddcb153d08ef91913981ab71c233ed027b63
Luke12345678909 <31909239+Luke12345678909@users.noreply.github.com>
2017-12-16 12:27:44 -0800

    Make math functions avalible

---
+++ project.js
@@ -0,0 +1,16 @@
+var ZERO_CONST=0;
+var ONE_CONST=1;
+var TWO_CONST=2;
+var THREE_CONST=3;
+var FOUR_CONST=4;
+var FIVE_CONST=5;
+var SIX_CONST=6;
+var SEVEN_CONST=7;
+var EIGHT_CONST=8;
+var NINE_CONST=9;
+function CODE(){
+var INPUT=ZERO_CONST;//input to SuperScript
+return Math.binary(INPUT);
+}();
+//CODE returns SuperScript value 
+//note that you use CODE not CODE().
